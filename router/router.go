f4850f1d06c49f994d41946c03eada8cab3f5230
Romain Menke <romainmenke@gmail.com>
2018-06-06 19:25:04 +0300

    fix host

--- router/router.go
+++ router/router.go
@@ -19,6 +19,11 @@ func New() http.Handler {
 			return
 		}
 
+		if strings.HasPrefix(r.Host, "gist.github") {
+			githubGist.ServeHTTP(w, r)
+			return
+		}
+
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}))
 }
