13940e13d0c0283095d2e04688b21bd498ab19ea
Romain Menke <romainmenke@gmail.com>
2018-06-06 13:34:09 +0200

    github gist specific proxy

---
+++ proxy/gist.go
@@ -0,0 +1,41 @@
+package proxy
+
+import (
+	"context"
+	"net/http"
+	"path"
+	"time"
+
+	"github.com/romainmenke/rapip/reader"
+)
+
+func GithubGist() http.Handler {
+
+	client := http.Client{
+		Timeout: time.Second * 30,
+		Jar:     nil, // explicit nil, this is a shared client!!
+	}
+
+	router := http.Handler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*30)
+		defer cancel()
+
+		req, err := http.NewRequest("GET", "https://"+path.Join("gist.githubusercontent.com/", r.URL.Path), nil)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		req.WithContext(ctx)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		reader.Respond(w, r, resp)
+	}))
+
+	return router
+}
